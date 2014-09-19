92e88a407cf1963895ba0d95a5ceb78eec38cf22
Fabio Caccamo <fabio.caccamo@gmail.com>
2014-09-19 17:02:45 +0200

    add UIView+ColorOfPoint

---
+++ FCUtils/UIView+ColorOfPoint.h
@@ -0,0 +1,12 @@
+//
+//  UIView+ColorOfPoint.h
+//
+//  Created by Fabio Caccamo on 19/09/14.
+//  Copyright (c) 2014 Fabio Caccamo. All rights reserved.
+//
+
+@interface UIView (ColorOfPoint)
+
+- (UIColor *) colorOfPoint:(CGPoint)point;
+
+@end
